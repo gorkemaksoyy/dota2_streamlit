6fc999cf19643eb4c9e3ed11b8da82d533a93a0c
Görkem Aksoy <97803129+gorkemaksoyy@users.noreply.github.com>
2023-01-15 21:53:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #pip install streamlit
 import pandas as pd
 import streamlit as st
-rec_list = pd.read_excel("https://github.com/gorkemaksoyy/dota2_streamlit/blob/5700bffa2981c78e6cc0ee3e75da3cee73d01c3c/tavsiye_listesi.xlsx")
+rec_list = pd.read_excel("tavsiye_listesi.xlsx")
 rec_list.index = rec_list["Item_Names"]
 rec_list.drop("Item_Names", axis=1, inplace=True)
 hero_list = list(rec_list.columns)
