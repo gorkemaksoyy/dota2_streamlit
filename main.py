c1a794b20d86ddc8b4a6ec90a783df0485b77bb7
Görkem Aksoy <97803129+gorkemaksoyy@users.noreply.github.com>
2023-01-15 21:57:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #pip install streamlit
 import pandas as pd
 import streamlit as st
+from openpyxl import Workbook
 rec_list = pd.read_excel("tavsiye_listesi.xlsx")
 rec_list.index = rec_list["Item_Names"]
 rec_list.drop("Item_Names", axis=1, inplace=True)
